f435795a2ffc8609a3edded5845ed4b08d211ac2
AlexYMR <alexyaniel34@gmail.com>
2022-08-11 01:32:16 -0400

    Five

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #depends on CPG-userdata-exported variables
+sudo apt-get -y update
 #install git
-apt-get -y install git-core
+sudo apt-get -y install git-core
 #install Apache Web Server on Ubuntu 16 LTS
-apt-get -y install apache2
+sudo apt-get -y install apache2
 #set permissions on the html folder so files can be copied
-chmod 777 var/www/html/
+sudo chmod 777 var/www/html/
 #download website files from git
-git clone https://github.com/AlexYMR/cpg-milestone
+sudo git clone https://github.com/AlexYMR/cpg-milestone
 #copy the downloaded content to the Apache www directory
 mv /cpg-milestone/*.* /var/www/html
 mv /cpg-milestone/css /var/www/html
 mv /cpg-milestone/img /var/www/html
 #restart the webserver
-systemctl start apache2.service
+sudo systemctl start apache2.service
 #replace placeholder text in static website page with user input
 sed -i "s,$WEB_TITLE,WEBPAGE_TITLE,g" /var/www/html/index.html
 sed -i "s,$WEB_HEADER,WEBPAGE_HEADER,g" /var/www/html/index.html
