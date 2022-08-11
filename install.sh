fcc4ff7c86c49ffa1e1bae883db458fa67f1ce58
AlexYMR <alexyaniel34@gmail.com>
2022-08-11 01:10:44 -0400

    Third

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#depends on CPG-userdata-exported variables
+#install git
+apt-get -y install git-core
+#install Apache Web Server on Ubuntu 16 LTS
+apt-get -y install apache2
+#set permissions on the html folder so files can be copied
+chmod 777 var/www/html/
+#download website files from git
+git clone https://github.com/AlexYMR/cpg-milestone
+#copy the downloaded content to the Apache www directory
+mv /cpg-milestone/*.* /var/www/html
+mv /cpg-milestone/css /var/www/html
+mv /cpg-milestone/img /var/www/html
+#restart the webserver
+systemctl start apache2.service
+#replace placeholder text in static website page with user input
+sed -i "s,$WEB_TITLE,WEBPAGE_TITLE,g" /var/www/html/index.html
+sed -i "s,$WEB_HEADER,WEBPAGE_HEADER,g" /var/www/html/index.html
+sed -i "s,$WEB_SUBHEADER,WEBPAGE_SUBHEADER,g" /var/www/html/index.html
+sed -i "s,url(\'../img/$WEB_BACKGROUND.jpg\'),url(\'../img/milestone.jpg\'),g" /var/www/html/index.html
